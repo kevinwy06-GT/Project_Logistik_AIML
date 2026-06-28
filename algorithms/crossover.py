025ee2d71c140187b6ff692525021312870ed629
C14240104 <c14240104@john.petra.ac.id>
2026-06-29 02:47:02 +0700

    update def __ordered_crossover di crossover.py

--- algorithms/crossover.py
+++ algorithms/crossover.py
@@ -79,40 +79,64 @@ class Crossover:
 
         return child1, child2
 
+    # @staticmethod
+    # def __ordered_crossover(parent_main,
+    #                         parent_donor,
+    #                         start,
+    #                         end):
+    #     """
+    #     Ordered Crossover (OX).
+
+    #     Used only for priority permutation.
+    #     """
+
+    #     size = len(parent_main)
+
+    #     child = [None] * size
+
+    #     # Copy middle segment
+    #     child[start:end] = parent_main[start:end]
+
+    #     current_position = end
+
+    #     for gene in parent_donor:
+
+    #         if gene not in child:
+
+    #             if current_position >= size:
+    #                 current_position = 0
+
+    #             while child[current_position] is not None:
+
+    #                 current_position += 1
+
+    #                 if current_position >= size:
+    #                     current_position = 0
+
+    #             child[current_position] = gene
+
+    #     return child
+    
     @staticmethod
-    def __ordered_crossover(parent_main,
-                            parent_donor,
-                            start,
-                            end):
-        """
-        Ordered Crossover (OX).
-
-        Used only for priority permutation.
-        """
-
+    def __ordered_crossover(parent_main, parent_donor, start, end):
         size = len(parent_main)
-
         child = [None] * size
 
-        # Copy middle segment
         child[start:end] = parent_main[start:end]
+        
+        used_genes = set(parent_main[start:end])
 
-        current_position = end
+        current_position = 0
 
         for gene in parent_donor:
-
-            if gene not in child:
-
-                if current_position >= size:
-                    current_position = 0
-
-                while child[current_position] is not None:
-
+            if gene not in used_genes:
+                
+                while current_position < size and child[current_position] is not None:
                     current_position += 1
-
-                    if current_position >= size:
-                        current_position = 0
-
-                child[current_position] = gene
+                
+                if current_position < size:
+                    child[current_position] = gene
+                    used_genes.add(gene)
 
         return child
+
